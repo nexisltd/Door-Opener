22f6d254760a7bd8b576abd56f6e6ecd347d875c
naser-rifat <rifatmahmud971@gmail.com>
2022-07-04 12:02:59 +0600

    app.js added

---
+++ src/static/Js/app.js
@@ -0,0 +1,23 @@
+const open = document.getElementById("open");
+
+open.addEventListener("click", () => {
+  fetch("/door");
+  document.getElementById("close").style.color = "red";
+  document.getElementById("open").style.background = "red";
+  document.getElementById("open").innerHTML = "Opened";
+  let timeLeft = 10;
+  let closeTimer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(closeTimer);
+    }
+    // let left = (document.getElementById("timer").innerText = 10 - timeLeft);
+    document.getElementById("close").innerText =
+      "door close in " + (10 - timeLeft) + " second";
+    // document.getElementById("progressBar").value = 10 - timeLeft;
+    timeLeft -= 1;
+    if (timeLeft < 0) {
+      document.getElementById("close").innerHTML = "";
+      document.getElementById("open").style.background = "#60A5FA";
+    }
+  }, 1000);
+});
