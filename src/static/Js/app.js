de2de7f6da2eacb221e101214d6071a0b3c93cec
naser-rifat <rifatmahmud971@gmail.com>
2022-07-04 15:49:34 +0600

    style update

--- src/static/Js/app.js
+++ src/static/Js/app.js
@@ -1,24 +1,33 @@
-const open = document.getElementById("open");
+const open = document.getElementById("openBtn");
 
 open.addEventListener("click", () => {
   fetch("/door");
-  document.getElementById("close").style.color = "red";
-  document.getElementById("open").style.background = "red";
-  document.getElementById("open").innerHTML = "Opened";
+  document.getElementById("openBtn").disabled = true;
+  document.getElementById("doorOpenCmdText").innerHTML = "";
+  document.getElementById("closeText").style.color = "red";
+  // document.getElementById("powerIcon").style.color = "#fff";
+  document.getElementById("openBtn").style.background = "#f1f5f9";
+  document.getElementById("openBtn").style.opacity = 0.5;
+  document.getElementById("openBtn").innerHTML =
+    '<i class="fa-solid fa-power-off text-2xl"></i>';
   let timeLeft = 10;
   let closeTimer = setInterval(function () {
     if (timeLeft <= 0) {
       clearInterval(closeTimer);
     }
     // let left = (document.getElementById("timer").innerText = 10 - timeLeft);
-    document.getElementById("close").innerText =
+    document.getElementById("closeText").innerText =
       "door close in " + (10 - timeLeft) + " second";
     // document.getElementById("progressBar").value = 10 - timeLeft;
     timeLeft -= 1;
     if (timeLeft < 0) {
-      document.getElementById("close").innerHTML = "";
-      document.getElementById("open").style.background = "#60A5FA";
-      document.getElementById("open").innerHTML = "Open";
+      document.getElementById("openBtn").disabled = false;
+      document.getElementById("openBtn").style.opacity = 1;
+
+      // document.getElementById("closeText").innerHTML = "";
+      document.getElementById("openBtn").style.background = "#e2e8f0";
+      document.getElementById("openBtn").innerHTML =
+        '<i class="fa-solid fa-power-off text-2xl"></i>';
     }
   }, 1000);
 });
