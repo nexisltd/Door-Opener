c854258dd8ee79c46aca83c0842777ec12577727
Istiak Hassan Emon <emon5122@gmail.com>
2022-07-06 13:25:08 +0600

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 
-gunicorn --bind 0.0.0.0:8000 door.wsgi -w 4 -k uvicorn.workers.UvicornWorker
+gunicorn --bind 0.0.0.0:8000 door.asgi -w 4 -k uvicorn.workers.UvicornWorker
