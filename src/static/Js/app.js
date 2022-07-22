1d2269e90258c23b5184420cfd101e222bdf43bf
ZèD <imzihad@gmail.com>
2022-07-22 16:34:15 +0600

    Updated scripts
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/static/Js/app.js
+++ src/static/Js/app.js
@@ -1,149 +1,87 @@
-const open = document.getElementById("openBtn");
-let url = `ws://${window.location.host}/ws/socket-server/`
-const doorSocket = new WebSocket(url)
+// Socket connect
+let url = `wss://${window.location.host}/ws/socket-server/`;
+const doorSocket = new WebSocket(url);
+
+// Element defined in HTML
+const openBtn = document.getElementById("openBtn");
+const doorOpenCmdText = document.getElementById("doorOpenCmdText");
+const closeText = document.getElementById("closeText");
 
 doorSocket.onmessage = function (e) {
-    let data = JSON.parse(e.data)
-    console.log('data : ', data)
-    if (data.type === 'door') {
-        if (data.message === 'Open') {
-            opened()
-        } else {
-            closed()
-        }
-    }
-}
+  let data = JSON.parse(e.data);
+  //   console.log(data);
 
-const opened = () => {
-    document.getElementById("openBtn").disabled = true;
-    document.getElementById("doorOpenCmdText").style.display = "none";
-    document.getElementById("openBtn").classList.remove('bg-gray-100')
-    document.getElementById("openBtn").classList.remove('cursor-pointer')
-    document.getElementById("openBtn").classList.add('bg-gray-500')
-    document.getElementById("openBtn").classList.add('cursor-not-allowed')
-    document.getElementById("closeText").style.color = "red";
-    let timeLeft = 10;
-    let closeTimer = setInterval(function () {
-        if (timeLeft <= 0) {
-            clearInterval(closeTimer);
-        }
-        document.getElementById("closeText").innerText =
-            "The door will close in " + timeLeft + " second.";
-        timeLeft -= 1;
-        if (timeLeft < 0) {
-            doorSocket.send(JSON.stringify({
-                'message': 'Close'
-            }))
-        }
-    }, 1000);
-}
+  if (data.type === "door") {
+    const msg = data?.message?.message;
+    if (msg !== "Close") {
+      msg !== "Open" && opened(msg);
+    } else {
+      closed();
+    }
+  }
+};
+
+const time = () => {
+  let timeLeft = 10;
+  let closeTimer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(closeTimer);
+    }
+    doorSocket.send(
+      JSON.stringify({
+        message: { message: timeLeft },
+      })
+    );
+    timeLeft -= 1;
+
+    if (timeLeft < 0) {
+      doorSocket.send(
+        JSON.stringify({
+          message: { message: "Close" },
+        })
+      );
+    }
+  }, 1000);
+  // return timeLeft;
+};
+
+const opened = (msg) => {
+  openBtn.disabled = true;
+  openBtn.classList.remove("bg-gray-100");
+  openBtn.classList.remove("cursor-pointer");
+  openBtn.classList.add("bg-gray-500");
+  openBtn.classList.add("cursor-not-allowed");
+  doorOpenCmdText.style.display = "none";
+  closeText.innerText = "The door will close in " + msg + " second.";
+  //   console.log(msg);
+  //   time();
+};
 
 const closed = () => {
-    document.getElementById("openBtn").disabled = false;
-    document.getElementById("closeText").innerHTML = "";
-    document.getElementById("openBtn").classList.remove('bg-gray-500')
-    document.getElementById("openBtn").classList.remove('cursor-not-allowed')
-    document.getElementById("openBtn").classList.add('bg-gray-100')
-    document.getElementById("openBtn").classList.add('cursor-pointer')
-    document.getElementById("doorOpenCmdText").style.display = "block";
-}
+  openBtn.disabled = false;
+  closeText.innerText = "";
+  openBtn.classList.remove("bg-gray-500");
+  openBtn.classList.remove("cursor-not-allowed");
+  openBtn.classList.add("bg-gray-100");
+  openBtn.classList.add("cursor-pointer");
+  doorOpenCmdText.style.display = "block";
+};
 
-open.addEventListener("click", () => {
-    fetch("/door")
-        .then(res => {
-            if (res.status === 200) {
-                doorSocket.send(JSON.stringify({
-                    'message': 'Open'
-                }))
-                opened()
-            }
-            return res.json()
-        })
-        .catch(err => {
-            console.log(err);
-        })
+openBtn.addEventListener("click", () => {
+  fetch("/door")
+    .then((res) => {
+      if (res.status === 200) {
+        doorSocket.send(
+          JSON.stringify({
+            message: { message: "Open" },
+          })
+        );
+        // opened(12);
+        time();
+      }
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-// const open = document.getElementById("openBtn");
-// let url = `ws://${window.location.host}/ws/socket-server/`
-// const doorSocket = new WebSocket(url)
-//
-// doorSocket.onmessage = function (e) {
-//     let data = JSON.parse(e.data)
-//     console.log('data : ', data)
-//
-//     if (data.type === 'door') {
-//         if (data.message === 'Open') {
-//             opened(data.message)
-//             // time()
-//
-//         } else {
-//             closed()
-//         }
-//     }
-// }
-//
-// const time = () => {
-//     let timeLeft = 11;
-//     let closeTimer = setInterval(function () {
-//         if (timeLeft <= 0) {
-//             clearInterval(closeTimer);
-//         }
-//         doorSocket.send(JSON.stringify({
-//             'message': {'message': timeLeft}
-//         }))
-//         timeLeft -= 1;
-//
-//         if (timeLeft < 0) {
-//             doorSocket.send(JSON.stringify({
-//                 'message': {'message': 'Close'},
-//             }))
-//         }
-//     }, 1000);
-//     // return timeLeft;
-//
-// }
-//
-// const opened = (msg) => {
-//     time()
-//     document.getElementById("openBtn").disabled = true;
-//     document.getElementById("doorOpenCmdText").style.display = "none";
-//     document.getElementById("openBtn").classList.remove('bg-gray-100')
-//     document.getElementById("openBtn").classList.remove('cursor-pointer')
-//     document.getElementById("openBtn").classList.add('bg-gray-500')
-//     document.getElementById("openBtn").classList.add('cursor-not-allowed')
-//     document.getElementById("closeText").style.color = "red";
-//     document.getElementById("closeText").innerText =
-//         "The door will close in " + msg +" second.";
-//     console.log(msg)
-//
-//
-//
-// }
-//
-// const closed = () => {
-//     document.getElementById("openBtn").disabled = false;
-//     document.getElementById("closeText").innerHTML = "";
-//     document.getElementById("openBtn").classList.remove('bg-gray-500')
-//     document.getElementById("openBtn").classList.remove('cursor-not-allowed')
-//     document.getElementById("openBtn").classList.add('bg-gray-100')
-//     document.getElementById("openBtn").classList.add('cursor-pointer')
-//     document.getElementById("doorOpenCmdText").style.display = "block";
-// }
-//
-// open.addEventListener("click", () => {
-//     fetch("/door")
-//         .then(res => {
-//             if (res.status === 200) {
-//                 doorSocket.send(JSON.stringify({
-//                     'message': {'message': 'Open'}
-//                 }))
-//                 opened()
-//                 time()
-//             }
-//             return res.json()
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-// });
-//
